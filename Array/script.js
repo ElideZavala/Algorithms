e541f2be23d4bc75f00fbb83b92559e14997516f
ElideZavala <zavalavinagreelide@gmail.com>
2021-06-30 19:35:03 -0600

    array

---
+++ Array/script.js
@@ -0,0 +1,13 @@
+const myArray = [11,3,23,7];
+
+myArray.push(17);  // => agregamos un nuevo elemento
+
+myArray.pop();     // => Quitamos el ultimo elemento
+
+// ==>  ambas son dos operaciones 0(1)
+
+myArray.shift()    // => Quitamos el primer elemento del array
+
+myArray.unshift(11);  // => Agregamos un elemento a la primera posicion del arreglo
+
+
