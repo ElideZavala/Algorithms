f403b2fc01e0619a8c240efba3f871d28e9e02d2
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-01 19:33:28 -0600

    arays

--- Array/script.js
+++ Array/script.js
@@ -10,4 +10,8 @@ myArray.shift()    // => Quitamos el primer elemento del array
 
 myArray.unshift(11);  // => Agregamos un elemento a la primera posicion del arreglo
 
+myArray.splice(1, 0, 'Hi'); // => Colocaremos algo en el indice 1, el zero indica que no eliminaremos ninguno y agregaremos un elemento Hi. 
 
+// ==> corresponde .splice a 0(n) === 0( 1/2n)
+
+// ==> Agregar y eliminar elementos desde el principio no es la mejor opcion 
