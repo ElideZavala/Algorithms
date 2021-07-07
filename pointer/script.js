3528232e44abc7471f13e559fb8d52cf46c390f8
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-07 11:10:01 -0600

    Link

---
+++ pointer/script.js
@@ -0,0 +1,20 @@
+// let num1 = 5;
+
+// let num2 = num1;
+
+
+
+let obj1 = {
+	value: 11
+}
+
+//El objeto 2 apuntara hacia el objeto1 y sise modifica este se modificara. 
+let obj2 = obj1;
+
+let obj3 = {
+	value: 57
+}
+
+obj2 = obj3;
+
+// Garbage Collection
