78aa9184a8e8cebbd5c4e3bb6e34f8a2ee19913d
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-11 19:32:24 -0600

    push

---
+++ 08-Push/push.js
@@ -0,0 +1,38 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		// create new Node 
+		const newNode = new Node(value)
+		this.head = newNode
+		this.tail = this.head
+		this.length = 1
+	}
+
+	TODO: // Si esta la es la cabeza agrega el nuevo elemento. 
+		push(value) {
+			const newNode = new Node(value)
+			if (!this.head) {
+				Note: // Un solo elemento
+					this.head = newNode
+				this.tail = newNode
+			}
+			else {
+				Note: // Al ultimo elemento. 
+					this.tail.next = newNode
+				this.tail = newNode
+			}
+			this.length++
+			return this
+		}
+}
+
+let myLinkedList = new LinkedList(7);
+MediaQueryList.push(4)
+
+console.log('%cpush.js line:4 cadena', 'color: #007acc;', cadena);
