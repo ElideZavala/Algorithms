0dce92f431012f14ca58797f26df690c93d578dc
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-23 19:55:07 -0600

    insert

--- 14-Insert/insert.js
+++ 14-Insert/insert.js
@@ -42,9 +42,27 @@ class LinkedList {
 		this.length++
 		return this
 	}
+	get(index) {
+		if (index < 0 || index >= this.length) {
+			return undefined
+		}
+		let temp = this.head
+		for (let i = 0; i < index; i++) {
+			temp = temp.next
+		}
+		return temp
+    	}
 	insert(index, value) {
 		if (index === 0) return this.unshift(value)  // <== Si no tenemos indice lo colocamos al inicio del arreglo.
 		if (index === this.length) return this.push(value) // <== Si el indice es igual a la logitud del arreglo lo colomos al ultimo. 
-		if (index < 0 || index > this.length) return false // <== retornar false si el indice no entra al inicio o al final del arreglo. 
+		if (index < 0 || index > this.length) return false // <== retornar false por que el index es menor a 0 a mayor al arreglo.
+		
+		const newNode = new Node(value)
+		const temp = this.get(index - 1)
+
+		newNode.next = temp.next
+		temp.next = newNode
+		this.length++
+		return true
     }
 }
