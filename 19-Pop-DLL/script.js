7d84e4ae257509a6939f1ee16a1c43b08bef917e
ElideZavala <zavalavinagreelide@gmail.com>
2021-07-31 18:30:03 -0600

    pop-dll

---
+++ 19-Pop-DLL/script.js
@@ -0,0 +1,50 @@
+class Node {
+	constructor(value) {
+		this.value = value
+		this.next = null
+		this.prev = null
+	}
+}
+
+class DoublyLinkedList {
+	constructor(value) {
+		const newNode = new Node(value)
+		this.head = newNode
+		this.tail = this.head
+		this.lenght = 1
+	}
+	push(value) {
+		const newNode = new Node(value)
+		if (!this.head) {
+			this.head = newNode
+			this.tail = newNode
+		} else {
+			this.tail.next = newNode
+			newNode.prev = this.tail
+			this.tail = newNode
+		}
+		this.lenght++
+		return this
+	}
+	pop() {
+		if( !this.lenght === 0) return undefined
+		let temp = this.tail 
+		if (this.lenght === 1) {
+			this.head = null
+			this.tail = null 
+		} else { 
+			this.tail = this.tail.prev
+			this.tail.next = null
+			temp.prev = null 
+		}
+		this.lenght-- 
+		return temp
+	}
+}
+
+
+let myDoublyLinkedList = new DoublyLinkedList(1);
+myDoublyLinkedList.push(2)
+myDoublyLinkedList.pop()
+myDoublyLinkedList.pop()
+console.log(myDoublyLinkedList)
