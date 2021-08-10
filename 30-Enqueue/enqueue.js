069138b9f55d9cc025d484078b155d1fdae1616d
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-10 10:37:15 -0600

    enqueue

--- 30-Enqueue/enqueue.js
+++ 30-Enqueue/enqueue.js
@@ -24,8 +24,22 @@ class Queue {
 		this.length++ 
 		return this
 	}
+	dequeue() {
+		if (this.length === 0) return undefined
+		let temp = this.first
+		if (this.length === 1) {
+			this.first = null
+			this.last  = null 
+		} else {
+			this.first = this.first.next
+			temp.next = null 
+		}
+		this.length--
+		return temp
+	}
 }
 
 let myQueue = new Queue(11)
 myQueue.enqueue(3)
 myQueue.enqueue(23)
+myQueue.enqueue(7)
