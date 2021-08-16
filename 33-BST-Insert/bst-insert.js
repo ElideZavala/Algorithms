bbffc78a1915bc27c30b8b83d1944bebeca919f9
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-15 19:38:44 -0600

    insert one

--- 33-BST-Insert/bst-insert.js
+++ 33-BST-Insert/bst-insert.js
@@ -1,1 +1,30 @@
-class insert
+class Node {
+	constructor(value) {
+		this.value = value
+		this.left = null
+		this.right = null
+	}
+}
+
+class BST {
+	constructor() {
+		this.root = null
+	} 
+	insert(value) {
+		const newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode 
+			return this
+		}
+		let temp = this.root
+		while(true) {
+			if (newNode.value === temp.value) return undefined 
+			if (newNode.value < temp.value) {
+				if (temp.left = null)
+				temp.left = newNode
+				return this
+			}
+			temp = temp.left
+		}
+	}
+}
