6b305a999dba936a40b2de1398b28c5b102edbc4
ElideZavala <zavalavinagreelide@gmail.com>
2021-08-26 19:29:32 -0600

    intruduction to graph

--- 39-HT-interviewQuestion/ht-interview-question.js
+++ 39-HT-interviewQuestion/ht-interview-question.js
@@ -1,13 +1,26 @@
-function itemInCommon(arr1, arr2) {
-	for(let i = 0; i < arr1.length; i++) {
-		for(let j = 0; j < arr2.length; j++) {
-			if(arr1[i] === arr2[j]) return true
-		}
+//
+// function itemInCommon(arr1, arr2) {
+// 	for(let i = 0; i < arr1.length; i++) {
+// 		for(let j = 0; j < arr2.length; j++) {
+// 			if(arr1[i] === arr2[j]) return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// let array1 = [1, 3, 5]
+// let array2 = [2, 4, 5]
+
+// itemInCommon(array1, array2)
+
+// Metodo mas eficiente
+function itemInnCommon(arr1, arr2) {
+	let obj = {}
+	for (let i = 0; i < arr1.length; i++) {
+		obj[arr1[i]] = true
+	}
+	for(let j = 0; j < arr2.length; j++) {
+		if (obj[arr2[j]]) return true
 	}
 	return false
 }
-
-let array1 = [1, 3, 5]
-let array2 = [2, 4, 5]
-
-itemInCommon(array1, array2)
