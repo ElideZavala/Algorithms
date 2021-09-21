8a5b9d41511b0b69713bf0947717301a9a5f934d
ElideZavala <zavalavinagreelide@gmail.com>
2021-09-21 09:37:07 -0600

    quick-sort

--- 50-Quick-Sort/quick-soft.js
+++
@@ -1,8 +0,0 @@
-function quicksoft(array, left=0, right=array.lenght-1) {
-	if(left < right) {
-		let pivotIndex = pivot(array, left, right)
-		quickSort(array, left, pivotIndex+1,)
-		quicksoft(array, pivotIndex+1, right)
-	}
-
-}
